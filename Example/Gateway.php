50cabcc1ede079631a726c51850ccdd77ba6c464
Mubeen142 <mubeenullah323@gmail.com>
2025-01-29 20:05:29 +0200

    init

---
+++ Example/Gateway.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Gateways\Example;
+
+use Illuminate\Http\Request;
+use LaraPay\Framework\Foundation\GatewayFoundation;
+
+class Gateway extends GatewayFoundation
+{
+    /**
+     * Define the gateway identifier. This identifier should be unique. For example,
+     * if the gateway name is "PayPal Express", the gateway identifier should be "paypal-express".
+     *
+     * @var string
+     */
+    protected string $identifier = 'example_gateway';
+
+    /**
+     * Define the gateway version.
+     *
+     * @var string
+     */
+    protected string $version = '1.0.0';
+
+    /**
+     * Define the currencies supported by this gateway
+     *
+     * @var array
+     */
+    protected array $currencies = [
+        'USD',
+        'EUR'
+    ];
+
+    public function config(): array
+    {
+        return [
+            'public_key' => [
+                'label' => 'Public Key',
+                'description' => 'Stripe Public Key',
+                'type' => 'text',
+                'rules' => ['required', 'numeric'],
+            ],
+        ];
+    }
+
+    public function pay($payment)
+    {
+        dd('Processing Payment using stripe', $payment);
+    }
+
+    public function callback(Request $request)
+    {
+
+    }
+}
